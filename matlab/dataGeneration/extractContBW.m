2da94a92837a1f7d475e668c6c9950a267d84606
xikang <zhangxk2009@gmail.com>
2014-10-11 00:40:36 -0400

    extractContBW updated

--- matlab/dataGeneration/extractContBW.m
+++ matlab/dataGeneration/extractContBW.m
@@ -4,28 +4,50 @@ function contour = extractContBW(BW)
 
 contour = {};
 k = 1;
-min_len = 10;
+minLen = 10;
 
 % delete trivial endpoint
-contourz_all = bwboundaries(BW, 4);
-len = cellfun(@length, contourz_all);
-contourz_all(len < 5) = [];
-for i = 1:length(contourz_all)
-    contourz = contourz_all{i};
-    hit = false(size(contourz, 1), 1);
-    d1 = sum(abs(contourz(1:end-2,:) - contourz(3:end,:)), 2);
-    d2 = sum(abs(contourz(1:end-4,:) - contourz(5:end,:)), 2);
-    d3 = sum(abs(contourz(2,:) - contourz(end-1,:)));
-    d4 = sum(abs(contourz(3,:) - contourz(end-2,:)));
-    hit(3:end-2) = (d1(2:end-1)==0 & d2~=0);
-    hit(2) = (d1(1)==0);
-    hit(end-1) = (d1(end)==0);
-    hit(1) = (d3==0 && d4~=0);
-    hit(end) = hit(1);
-    ind = find(hit);
-    indToErase = sub2ind(size(BW), contourz(ind,1), contourz(ind,2));
-    BW(indToErase) = 0;
-end
+cont_filter1 = [
+    0 0 0;
+    0 1 0;
+    1 1 1];
+cont_filter2 = [
+    1 1 1;
+    1 0 1;
+    0 0 0];
+tmp_bw1 = conv2(BW, cont_filter1, 'same');
+tmp_bw2 = conv2(BW, cont_filter2, 'same');
+BW(tmp_bw1==4 & tmp_bw2==0) = 0;
+tmp_bw1 = conv2(BW, rot90(cont_filter1), 'same');
+tmp_bw2 = conv2(BW, rot90(cont_filter2), 'same');
+BW(tmp_bw1==4 & tmp_bw2==0) = 0;
+tmp_bw1 = conv2(BW, flipud(cont_filter1), 'same');
+tmp_bw2 = conv2(BW, flipud(cont_filter2), 'same');
+BW(tmp_bw1==4 & tmp_bw2==0) = 0;
+tmp_bw1 = conv2(BW, rot90(flipud(cont_filter1)), 'same');
+tmp_bw2 = conv2(BW, rot90(flipud(cont_filter2)), 'same');
+BW(tmp_bw1==4 & tmp_bw2==0) = 0;
+
+figure;imshow(BW);
+% contourz_all = bwboundaries(BW, 4);
+% len = cellfun(@length, contourz_all);
+% contourz_all(len < 5) = [];
+% for i = 1:length(contourz_all)
+%     contourz = contourz_all{i};
+%     hit = false(size(contourz, 1), 1);
+%     d1 = sum(abs(contourz(1:end-2,:) - contourz(3:end,:)), 2);
+%     d2 = sum(abs(contourz(1:end-4,:) - contourz(5:end,:)), 2);
+%     d3 = sum(abs(contourz(2,:) - contourz(end-1,:)));
+%     d4 = sum(abs(contourz(3,:) - contourz(end-2,:)));
+%     hit(3:end-2) = (d1(2:end-1)==0 & d2~=0);
+%     hit(2) = (d1(1)==0);
+%     hit(end-1) = (d1(end)==0);
+%     hit(1) = (d3==0 && d4~=0);
+%     hit(end) = hit(1);
+%     ind = find(hit);
+%     indToErase = sub2ind(size(BW), contourz(ind,1), contourz(ind,2));
+%     BW(indToErase) = 0;
+% end
 
 % contourz_all = bwboundaries(BW, 8);
 % len = cellfun(@length, contourz_all);
@@ -185,20 +207,20 @@ for i = 1:length(contourz_all)
                     break;
                 end
             end
-            % add to contour if its length > maxLen
-            if((ind_nxt-ind_pre)/2 >= min_len)
+            % add to contour if its length > minLen
+%             if((ind_nxt-ind_pre)/2 >= minLen)
                 contourz_firsthalf = contourz(ind_pre+1:ind, :);
                 contourz_secondhalf = contourz(ind:ind_nxt-1, :);
                 contour{k} = contourz_firsthalf;
                 k = k + 1;
-            end
+%             end
             % remove the segment while keep the connection
             contourz(ind_pre+2:ind_nxt-1, :) = [];
             if size(contourz, 1) == 1
                 contourz = [];
             end
         else
-            if size(contourz, 1) >= min_len
+            if size(contourz, 1) >= minLen
                 contour{k} = contourz;
                 k = k + 1;
             end
