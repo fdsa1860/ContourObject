002a11c9707f3ba46b11d18bdca6add19d7891a4
xikang <zhangxk2009@gmail.com>
2014-10-13 17:40:27 -0400

    bug fixed

--- matlab/test_objectDetection_sigma_lineSeparate.m
+++ matlab/test_objectDetection_sigma_lineSeparate.m
@@ -17,19 +17,19 @@ negDir = sprintf('../../../data/INRIAPerson/%s/neg/', opt);
 [imgList, labels] = loadImgList(posDir, negDir);
 
 %% compute contours, then features
-tic
-[dscA_all, seg_all, imgSize_all] = img2dscaAll(imgList, opt, false, true);
-toc
-save(sprintf('dscaSeg_%s_20141012', opt), 'dscA_all', 'seg_all', 'imgSize_all', 'labels');
+% tic
+% [dscA_all, seg_all, imgSize_all] = img2dscaAll(imgList, opt, false, true);
+% toc
+% save(sprintf('dscaSeg_%s_20141012', opt), 'dscA_all', 'seg_all', 'imgSize_all', 'labels');
 load(sprintf('../expData/dscaSeg_%s_20141012', opt));
 
 %% filter the short curves
 [dscA_all, seg_all, dscA_ind] = filterWithFixedLengthAll(dscA_all, seg_all, 2*hankel_size);
 
 %% slide window crop contours
-% [dscA_all, seg_all, points_all] = slideWindowChopContourAll(dscA_all, seg_all, 2*hankel_size, true);
+[dscA_all, seg_all, points_all] = slideWindowChopContourAll(dscA_all, seg_all, 2*hankel_size, true);
 % save(sprintf('dscASeg_%s_sw_20141005', opt), 'dscA_all', 'seg_all', 'points_all');
-load(sprintf('../expData/dscASeg_%s_sw_20141005', opt));
+% load(sprintf('../expData/dscASeg_%s_sw_20141005', opt));
 
 %% line detection
 isLine_all = dscaLineDetectAll(dscA_all);
@@ -38,14 +38,14 @@ isLine_all = dscaLineDetectAll(dscA_all);
 [dscA_line_all, dscA_notLine_all, seg_line_all, seg_notLine_all, points_line_all, points_notLine_all] = separateLine(dscA_all, seg_all, points_all, isLine_all, true);
 
 %% build hankel matrix
-% [dscA_notLine_all_H, dscA_notLine_all_HH] = buildHankelAll(dscA_notLine_all, hankel_size, 1, true);
+[dscA_notLine_all_H, dscA_notLine_all_HH] = buildHankelAll(dscA_notLine_all, hankel_size, 1, true);
 % save(sprintf('HH_dscA_notLine_%s_20141005', opt), 'dscA_notLine_all_H', 'dscA_notLine_all_HH');
-load(sprintf('../expData/HH_dscA_notLine_%s_20141005', opt));
+% load(sprintf('../expData/HH_dscA_notLine_%s_20141005', opt));
 
 %% normalized singular value estimation
-% dscA_notLine_all_sigma = sigmaEstAll(dscA_notLine_all_H, []);
+dscA_notLine_all_sigma = sigmaEstAll(dscA_notLine_all_H, []);
 % save(sprintf('sigma_dscA_notLine_%s_20141005', opt), 'dscA_notLine_all_sigma');
-load(sprintf('../expData/sigma_dscA_notLine_%s_20141005', opt));
+% load(sprintf('../expData/sigma_dscA_notLine_%s_20141005', opt));
 
 %% pooling
 sampleNum = 10000;
@@ -57,11 +57,11 @@ poolMaxSize = 50000;
 %% computer cluster centers
 nc = 100;
 % load ../expData/ped_dscA_notLine_sD_a0_20141005
-% tic;
-% [sLabel, centers, centers_sigma, centers_H, centers_HH, sD, centerInd] = nCutContourHHSigma(dscANotLinePool(1:10000), dscANotLinePoolSigma(:, 1:10000), dscANotLinePoolH(1:10000), dscANotLinePoolHH(1:10000), nc, alpha, sD);
-% toc
-% save ped_dscA_notLine_sD_a0_20141005 sD;
-% save ped_dscA_notLine_centers100_a0_20141005 centers centers_sigma centers_H centers_HH centerInd sLabel;
+tic;
+[sLabel, centers, centers_sigma, centers_H, centers_HH, sD, centerInd] = nCutContourHHSigma(dscANotLinePool(1:10000), dscANotLinePoolSigma(:, 1:10000), dscANotLinePoolH(1:10000), dscANotLinePoolHH(1:10000), nc, alpha);
+toc
+save ped_dscA_notLine_sD_a0_20141005 sD;
+save ped_dscA_notLine_centers100_a0_20141012 centers centers_sigma centers_H centers_HH centerInd sLabel;
 load ../expData/ped_dscA_notLine_centers100_a0_20141005
 
 %% bow representation
