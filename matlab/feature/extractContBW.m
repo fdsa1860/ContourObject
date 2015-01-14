4596615be1a5df9c9177315908d27ab415978947
Xikang Zhang <zhangxk2009@gmail.com>
2015-01-14 17:00:54 -0500

    test_BSDS500.m added; missing mexa64 files added; mengran's original version extractContBW.m added

--- matlab/feature/extractContBW.m
+++ matlab/feature/extractContBW.m
@@ -2,215 +2,103 @@
 
 function contour = extractContBW(BW)
 
-k = 1;
-minLen = 5;
-maxContourNum = 10000;
-contour = cell(maxContourNum, 1);
+contour = {};
 
-% delete trivial endpoint
-cont_filter1 = [
-    0 0 0;
-    0 1 0;
-    1 1 1];
-cont_filter2 = [
-    1 1 1;
-    1 0 1;
-    0 0 0];
-tmp_bw1 = conv2(BW, cont_filter1, 'same');
-tmp_bw2 = conv2(BW, cont_filter2, 'same');
-BW(tmp_bw1==4 & tmp_bw2==0) = 0;
-tmp_bw1 = conv2(BW, rot90(cont_filter1), 'same');
-tmp_bw2 = conv2(BW, rot90(cont_filter2), 'same');
-BW(tmp_bw1==4 & tmp_bw2==0) = 0;
-tmp_bw1 = conv2(BW, rot90(cont_filter1, 2), 'same');
-tmp_bw2 = conv2(BW, rot90(cont_filter2, 2), 'same');
-BW(tmp_bw1==4 & tmp_bw2==0) = 0;
-tmp_bw1 = conv2(BW, rot90(cont_filter1, 3), 'same');
-tmp_bw2 = conv2(BW, rot90(cont_filter2, 3), 'same');
-BW(tmp_bw1==4 & tmp_bw2==0) = 0;
+cont_filter = [0 1 0;
+                     1 1 0;
+                     0 0 0];
+tmp_bw = conv2(BW, cont_filter, 'same');
+BW(tmp_bw == 3) = 0;
+              
+cont_filter = [0 1 0;
+                     0 1 1;
+                     0 0 0];
+tmp_bw = conv2(BW, cont_filter, 'same');
+BW(tmp_bw == 3) = 0;
+              
+cont_filter = [0 0 0;
+                     1 1 0;
+                     0 1 0];
+tmp_bw = conv2(BW, cont_filter, 'same');
+BW(tmp_bw == 3) = 0;
+              
+cont_filter = [0 0 0;
+                     0 1 1;
+                     0 1 0];
+tmp_bw = conv2(BW, cont_filter, 'same');
+BW(tmp_bw == 3) = 0;
 
-cont_filter3 = [
-    0 1 0;
-    1 1 0;
-    0 0 0];
-cont_filter4 = [
-    1 1 1;
-    1 0 1;
-    1 1 1];
-tmp_bw3 = conv2(BW, cont_filter3, 'same');
-tmp_bw4 = conv2(BW, cont_filter4, 'same');
-[r, c] = find(tmp_bw3==3);
-n = length(r);
-Index = [sub2ind(size(BW),r,c) sub2ind(size(BW),r+1,c) sub2ind(size(BW),r,c+1)];
-M = tmp_bw4(Index);
-[~, ind] = min(M, [], 2);
-I = sparse(1:n, ind, true, n, 3);
-BW(Index(I)) = 0;
-
-tmp_bw3 = conv2(BW, rot90(cont_filter3), 'same');
-tmp_bw4 = conv2(BW, cont_filter4, 'same');
-[r, c] = find(tmp_bw3==3);
-n = length(r);
-Index = [sub2ind(size(BW),r,c) sub2ind(size(BW),r-1,c) sub2ind(size(BW),r,c+1)];
-M = tmp_bw4(Index);
-[~, ind] = min(M, [], 2);
-I = sparse(1:n, ind, true, n, 3);
-BW(Index(I)) = 0;
-
-tmp_bw3 = conv2(BW, rot90(cont_filter3, 2), 'same');
-tmp_bw4 = conv2(BW, cont_filter4, 'same');
-[r, c] = find(tmp_bw3==3);
-n = length(r);
-Index = [sub2ind(size(BW),r,c) sub2ind(size(BW),r-1,c) sub2ind(size(BW),r,c-1)];
-M = tmp_bw4(Index);
-[~, ind] = min(M, [], 2);
-I = sparse(1:n, ind, true, n, 3);
-BW(Index(I)) = 0;
-
-tmp_bw3 = conv2(BW, rot90(cont_filter3, 3), 'same');
-tmp_bw4 = conv2(BW, cont_filter4, 'same');
-[r, c] = find(tmp_bw3==3);
-n = length(r);
-Index = [sub2ind(size(BW),r,c) sub2ind(size(BW),r+1,c) sub2ind(size(BW),r,c-1)];
-M = tmp_bw4(Index);
-[~, ind] = min(M, [], 2);
-I = sparse(1:n, ind, true, n, 3);
-BW(Index(I)) = 0;
-
-% delete trivial endpoint
-contourz_all = bwboundaries(BW, 8);
-len = cellfun(@length, contourz_all);
-contourz_all(len < 5) = [];
-for i = 1:length(contourz_all)
-    contourz = contourz_all{i};
-    hit = false(size(contourz, 1), 1);
-    d1 = sum(abs(contourz(1:end-2,:) - contourz(3:end,:)), 2);
-    d2 = sum(abs(contourz(1:end-4,:) - contourz(5:end,:)), 2);
-    d3 = sum(abs(contourz(2,:) - contourz(end-1,:)));
-    d4 = sum(abs(contourz(3,:) - contourz(end-2,:)));
-    hit(3:end-2) = (d1(2:end-1)==0 & d2~=0);
-    hit(2) = (d1(1)==0);
-    hit(end-1) = (d1(end)==0);
-    hit(1) = (d3==0 && d4~=0);
-    hit(end) = hit(1);
-    ind = find(hit);
-    if ~isempty(ind)
-        indToErase = sub2ind(size(BW), contourz(ind,1), contourz(ind,2));
-        BW(indToErase) = 0;
-    end
-end
-
-% acquire the contours
-cont_filter = [
-    1 1 1;
-    1 0 1;
-    1 1 1];
+% Break the contours at crossing
+cont_filter = [1 1 1;
+                     1 0 1;
+                     1 1 1];
 tmp_bw = conv2(BW, cont_filter, 'same');
 tmp_bw = tmp_bw.*BW;
-[row, col] = find(tmp_bw == 1);
-endpoint = [row col];
-[row, col]=find(tmp_bw > 2);
-crosspoint = [row col];
+BW(tmp_bw >= 3) = 0;
 
+min_len = 20;
 
-% break at cross points
-contourz_all = bwboundaries(BW, 8);
-len = cellfun(@length, contourz_all);
-contourz_all(len < 5) = [];
-for i = 1:length(contourz_all)
-    
-    contourz = contourz_all{i};
-    % enforce contour to start and end with endpoint or crosspoint
-    isEndPt = ismember(contourz, endpoint, 'rows');
-    isCrossPt = ismember(contourz, crosspoint, 'rows');
-    if any(isEndPt)
-        offset = find(isEndPt, 1);
-        contourz = circshift(contourz(1:end-1, :), 1-offset);
-        contourz(end+1, :) = contourz(1, :);
-    elseif any(isCrossPt)
-        offset = find(isCrossPt, 1);
-        contourz = circshift(contourz(1:end-1, :), 1-offset);
-        contourz(end+1, :) = contourz(1, :);
-    end
+% find the beginning point of each trajectory
+cont_filter = [1 1 1;
+                     1 0 1;
+                     1 1 1];
+tmp_bw = conv2(BW, cont_filter, 'same');
+BW = tmp_bw.*BW;
 
-    isEndPt = ismember(contourz, endpoint, 'rows');
-    isCrossPt = ismember(contourz, crosspoint, 'rows');
-    if any(isCrossPt)
-        crossPtInd = find(isCrossPt);
-        endPtInd = find(isEndPt);
-        if isempty(endPtInd)
-            segInd = [crossPtInd(1:end-1) crossPtInd(2:end)];
-        else
-            segInd = zeros(nnz(isCrossPt), 2);
-            sortedInd = sort([crossPtInd; endPtInd]);
-            tmp = find(ismember(sortedInd, crossPtInd));
-            segInd(:, 1) = crossPtInd;
-            segInd(:, 2) = sortedInd(tmp + 1);
-        end
-        seg = cell(1, size(segInd, 1));
-        for j = 1:size(segInd, 1)
-            seg{j} = contourz(segInd(j, 1) : segInd(j, 2), :);
-        end
-        mergedSeg = mergeContour(seg);
-        nMerged = length(mergedSeg);
-        contour(k:k+nMerged-1) = mergedSeg;
-        k = k + nMerged;
-    elseif any(isEndPt)
-        contour{k} = contourz(1:round(size(contourz,1)/2), :);
-        k = k + 1;
+[row, col]=find(BW == 1);
+k=1;
+endpoint=[row';col']';
+
+[row, column]=find(BW > 1);
+all_p=[row'; column']';
+
+while (~isempty(all_p))
+    if isempty(endpoint)
+        % all_p store all the begining point
+        i = all_p(1,:);
+        contourz = bwtraceboundary(BW, i, 'W', 8, 2000,'clockwise');
+        % sepatate the contourz for two parts, and use both of them as the same
+        % trajectory from different direction
+        % contourz_half is the first half and contourz_half1 is the seconde half
+        % if there is an wheel in the image there is no beginning point we directly
+        % set contourz_half information to contourz
+        contourz_half=contourz;
+%         contourz_half1=[];
     else
-        contour{k} = contourz;
-        k = k + 1;
+        i=endpoint(1,:);
+        contourz = bwtraceboundary(BW, i, 'W', 8, 2000,'clockwise');
+        contourz_half=contourz(1:floor(length(contourz)/2),:);
+        %           contourz_half1=contourz(floor(length(contourz)/2)+1:length(contourz),:);
+        contourz = [contourz_half;
+            %                      contourz_half1;
+                    endpoint(1,:)];
+        
     end
-end
-contour(k:end) = [];
-
-% % break at endpoints
-% minLen = 10;
-% contourz_all = bwboundaries(BW, 8);
-% len = cellfun(@length, contourz_all);
-% contourz_all(len < 5) = [];
-% for i = 1:length(contourz_all)
-%     contourz = contourz_all{i};
-%     while ~isempty(contourz)
-%         indToErase = sub2ind(size(BW), contourz(:,1), contourz(:,2));
-%         BW(indToErase) = 0;
-%         isValid = ismember(contourz, endpoint, 'rows') & ~ismember(contourz, contourz(1,:), 'rows');
-%         ind = find(isValid, 1);
-%         if ~isempty(ind)
-%             ind_pre = ind;
-%             ind_nxt = ind;
-%             while  all(contourz(ind_pre, :) == contourz(ind_nxt, :))
-%                 ind_pre = ind_pre - 1;
-%                 ind_nxt = ind_nxt + 1;
-%                 if ind_pre < 1 || ind_nxt > size(contourz, 1)
-%                     break;
-%                 end
-%             end
-%             % add to contour if its length > minLen
-% %             if((ind_nxt-ind_pre)/2 >= minLen)
-%                 contourz_firsthalf = contourz(ind_pre+1:ind, :);
-%                 contourz_secondhalf = contourz(ind:ind_nxt-1, :);
-%                 contour{k} = contourz_firsthalf;
-%                 k = k + 1;
-% %             end
-%             % remove the segment while keep the connection
-%             contourz(ind_pre+2:ind_nxt-1, :) = [];
-%             if size(contourz, 1) == 1
-%                 contourz = [];
-%             end
-%         else
-%             if size(contourz, 1) >= minLen
-%                 contour{k} = contourz;
-%                 k = k + 1;
-%             end
-%             contourz = [];
+    
+    %     estimate the contour is empty or not, if not the point in each
+    %     trajectory should longer than 15
+    if(~isempty(contourz))
+        if(size(contourz_half,1)>=min_len)
+            contour{k}=contourz_half;
+            k=k+1;
+        end
+%         if(size(contourz_half1,1)>=min_len)
+%             contour{k}=contourz_half1;
+%             k=k+1;
 %         end
-%     end
-% end
-
-
-len = cellfun(@length, contour);
-contour(len < minLen) = [];
+        
+        for n = 1:length(contourz)
+            BW(contourz(n,1),contourz(n,2)) = 0;
+        end
+        [row1,col1]=find(BW==1);
+        endpoint=[row1,col1];
+        [row,column]=find(BW > 1);
+        all_p=[row';column']';
+    else
+        all_p(1,:)=[];
+    end
+    
+end
+contour = contour';
 
 end
---
+++ matlab/test_BSDS500.m
@@ -0,0 +1,72 @@
+% test_BSDS500
+% get contours from training data, do clustering, then label the test
+% contours, output label maps
+
+%% set up environment
+clc;clear;close all;
+addpath(genpath('../3rdParty'));
+addpath(genpath('../matlab'));
+addpath(genpath('../mex'));
+
+dataDir = '/home/xikang/research/data/BSR/BSDS500/data';
+
+% parameters
+opt.hankel_size = 4;
+opt.sampleMode = 1;
+opt.sampleLen = 1;
+opt.minLen = 2 * opt.hankel_size + 2;
+opt.segLength = 2 * opt.hankel_size + 1;
+opt.subjectNum = 1;
+opt.alpha = 0;
+opt.draw = false;
+opt.verbose = true;
+
+% opt = 'mytrain';
+% opt = 'mytest';
+
+%% get file name list
+files = dir(fullfile(dataDir,'groundTruth','train','*.mat'));
+nTrain = length(files);
+trainFileNameList = cell(1, nTrain);
+for i = 1:nTrain
+    trainFileNameList{i} = fullfile(dataDir,'groundTruth','train',files(i).name);
+end
+
+%%
+seg_all = cell(1, nTrain);
+for i = 1:nTrain
+    t = importdata(trainFileNameList{i});
+    bw = t{opt.subjectNum}.Boundaries;
+    cont = extractContBW(single(bw));
+    contour = sampleAlongCurve(cont, opt.sampleMode, opt.sampleLen);
+    contour = filterContourWithFixedLength(contour, opt.segLength);
+    seg = slideWindowContour2Seg(contour, opt.segLength);
+    seg = addHH(seg);
+    seg = sigmaEst(seg);
+    seg_all{i} = seg;
+end
+
+%% pooling
+poolMaxSize = 50000;
+rng('default');
+numImg = length(seg_all);
+r = randperm(numImg);
+counter = 1;
+segPool = [];
+for i = 1:numImg
+    segPool = [segPool seg_all{r(i)}];
+    counter = counter + length(seg_all{r(i)});
+    if counter > poolMaxSize, break; end
+end
+
+%% computer cluster centers
+nc = 100;
+% load ../expData/ped_sD_a0_notClean_20141117;
+% tic;
+% [centers, sLabel, sD] = nCutContourHHSigma(segPool(1:10000), nc, opt.alpha);
+% toc
+% save ped_sD_a0_notClean_20141117 sD;
+% save ped_centers_w100_a0_sig001_20141117 centers sLabel;
+load ../expData/bsds_centers_w100_a0_sig001_20150114
+
+1
